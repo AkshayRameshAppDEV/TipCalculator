cd9134e742350e14a755c16cc7adae0b21aa089b
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-22 18:50:40 -0600

    Add center alignment

--- tip_calculator/lib/ui/ui_widgets/tip_calculator_app.dart
+++ tip_calculator/lib/ui/ui_widgets/tip_calculator_app.dart
@@ -18,6 +18,7 @@ class TipCalculatorApp extends StatelessWidget {
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const [
               TotalPerPerson(),
               BillTipControl(),
