698c56bea40c08bf1871df88222e2a5b59e0c518
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-23 11:18:42 -0600

    Add input keyboard

--- tip_calculator/lib/ui/ui_widgets/bill_tip_control_widget.dart
+++ tip_calculator/lib/ui/ui_widgets/bill_tip_control_widget.dart
@@ -12,46 +12,40 @@ class _BillTipControlWidgetState extends State<BillTipControlWidget> {
   Widget build(BuildContext context) {
     return Card(
       shape: RoundedRectangleBorder(
-        side:  const BorderSide(color: Colors.black, width: 0.5),
-        borderRadius: BorderRadius.circular(10.0)
-      ),
+          side: const BorderSide(color: Colors.black, width: 0.5),
+          borderRadius: BorderRadius.circular(10.0)),
       color: Colors.white,
-      child:  Center(
-        child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text( 
-              "Bill Amount", 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0, 
-                color: Colors.black
-              )
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextFormField(
+                onTap: () {
+                  FocusScopeNode currentFocus = FocusScope.of(context);
+
+                  if (!currentFocus.hasPrimaryFocus) {
+                    currentFocus.unfocus();
+                  }
+                },
+                keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true, signed: false),
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter the Amount in (\$)',
+                ),
+              ),
             ),
-            Text( 
-              "Split", 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0, 
-                color: Colors.black
-              )
-            ),
-            Text( 
-              "Tip", 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0, 
-                color: Colors.black
-              )
-            ),
-            Text( 
-              "Percentage Slider", 
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0, 
-                color: Colors.black
-              )
-            ),
+            const Text("Split",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, color: Colors.black)),
+            const Text("Tip",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, color: Colors.black)),
+            const Text("Percentage Slider",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20.0, color: Colors.black)),
           ],
         ),
       ),
