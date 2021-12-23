4633b2db79e5709e5149ecdccf840246ed27a7c2
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-22 23:17:37 -0600

    Implemented Total Per Person card widget UI

--- tip_calculator/lib/ui/ui_widgets/total_per_person_widget.dart
+++ tip_calculator/lib/ui/ui_widgets/total_per_person_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../ui_constants/ui_constants.dart';
 
 class TotalTipPerPersonWidget extends StatefulWidget {
   const TotalTipPerPersonWidget({Key? key}) : super(key: key);
@@ -10,6 +11,20 @@ class TotalTipPerPersonWidget extends StatefulWidget {
 class _TotalTipPerPersonWidgetState extends State<TotalTipPerPersonWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Total Per Person Widget");
+    return  SizedBox(
+      height: 200,
+      child: Card(
+        color: appThemeColor.shade100,
+        child:  Center(
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Total Per Person", style: TextStyle(fontSize: 20.0, color: appThemeColor)),
+              Text("\$91.67", style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: appThemeColor))
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
