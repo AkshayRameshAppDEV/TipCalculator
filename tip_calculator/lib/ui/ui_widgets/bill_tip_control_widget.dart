e848560c6c198e4f8e9bebf77712abe37ed97f41
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-24 13:05:21 -0600

    Add $ sign

--- tip_calculator/lib/ui/ui_widgets/bill_tip_control_widget.dart
+++ tip_calculator/lib/ui/ui_widgets/bill_tip_control_widget.dart
@@ -123,7 +123,7 @@ class _BillTipControlWidgetState extends State<BillTipControlWidget> {
             style: TextStyle(fontSize: 20.0, color: appThemeColor)),
         Row(
           children: [
-            Text('$tipAmount',
+            Text('\$$tipAmount',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     fontSize: 20.0,
