e2effa95a4099b3fe888514a0ce2903062d1ef90
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-25 16:24:29 -0600

    Add initial row of text

--- tip_calculator/lib/ui/ui_widgets/app_theme_color_changer.dart
+++ tip_calculator/lib/ui/ui_widgets/app_theme_color_changer.dart
@@ -10,6 +10,28 @@ class AppThemeColorChangerWidget extends StatefulWidget {
 class _AppThemeColorChangerWidgetState extends State<AppThemeColorChangerWidget> {
   @override
   Widget build(BuildContext context) {
-    return Text("App theme color Changer container");
+    return Card(
+      shape: RoundedRectangleBorder(
+          side: const BorderSide(color: Colors.black, width: 0.5),
+          borderRadius: BorderRadius.circular(10.0)),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Color Circle 1"),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Color Circle 2"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
